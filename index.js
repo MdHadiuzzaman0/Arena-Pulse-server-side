80092a550540e3fbbb6e2a9178b5a6ce830447b0
Mdhadiuzzaman0 <mdhadiuzzaman483@gmail.com>
2026-05-19 16:51:38 +0600

    feat: create POST endpoint to insert new facility into MongoDB

--- index.js
+++ index.js
@@ -20,26 +20,46 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-     const database = client.db("ArenaPulse");
+    const database = client.db("ArenaPulse");
     const facilityCollection = database.collection("facilities");
     // const bookingCollection = database.collection("bookings");
 
-     app.get('/facilities', async (req, res) => {
+    //get
+    app.get('/facilities', async (req, res) => {
       const result = await facilityCollection.find().toArray()
       res.json(result)
     })
 
+    //get by id
     app.get("/facilities/:id", async (req, res) => {
-          const id = req.params.id;
-          const idBSON = { _id: new ObjectId(id) }
-          const result = await facilityCollection.findOne(idBSON)
-          res.json(result)
-          console.log(result)
-        })
+      const id = req.params.id;
+      const idBSON = { _id: new ObjectId(id) }
+      const result = await facilityCollection.findOne(idBSON)
+      res.json(result)
+      // console.log(result)
+    })
+
+    //post
+    app.post("/facilities", async (req, res) => {
+      const newFacility = req.body;
+      const result = await facilityCollection.insertOne(newFacility)
+      // console.log(result)
+      res.json(result)
+    })
+
+    //get by email
+    app.get("/facilities/:id", async (req, res) => {
+    const param = req.params.id;
+    if (param.includes('@')) {
+        const email = param;
+        const result = await facilityCollection.find({owner_email: email}).toArray(); 
+        console.log("Fetched by email:", result);
+        return res.json(result);
+    }})
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } 
+  }
   finally {
     // await client.close();
   }
